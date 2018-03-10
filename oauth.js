cf3afca31f1d5392b9abdf9df1ec2b6d9ebfe015
juanmosv <3450624+juanmosv@users.noreply.github.com>
2018-03-10 14:58:09 -0600

    Update oauth.js

--- oauth.js
+++ oauth.js
@@ -17,24 +17,52 @@ window.onload = function() {
         'contentType': 'json'
       };
       fetch(
-          'https://www.google.com/m8/feeds/contacts/default/full',
+          'https://www.google.com/m8/feeds/contacts/default/full?alt=json',
           init)
           .then((response) => response.json())
           .then(function(data) {
             let photoDiv = document.querySelector('#friendDiv');
-            let returnedContacts = data.memberResourceNames;
-            for (var i = 0; i < returnedContacts.length; i++) {
-              fetch(
-                  'https://www.google.com/m8/feeds/' + returnedContacts[i] +
-                      '?personFields=photos&key=AIzaSyDbrFE2K4TVC-VlcIoY--X-o-4po2Ei72w',
-                  init)
-                  .then((response) => response.json())
-                  .then(function(data) {
-                    let profileImg = document.createElement('img');
-                    profileImg.src = data.photos[0].url;
-                    photoDiv.appendChild(profileImg);
-                  });
-            };
+            var contacts = [];
+	          var data = data;
+            	  for (var i = 0, entry; entry = data.feed.entry[i]; i++) {
+	    var contact = {
+	      'name' : entry['title']['$t'],
+	      'id' : entry['id']['$t'],
+	      'emails' : []
+	    };
+                  if (entry['gd$email']) {
+	      var emails = entry['gd$email'];
+	      for (var j = 0, email; email = emails[j]; j++) {
+	        contact['emails'].push(email['address']);
+	      }
+	    }
+	
+	    if (!contact['name']) {
+	      contact['name'] = contact['emails'][0] || "<Unknown>";
+	    }
+	    contacts.push(contact);
+   
+       //Add divs contacts info
+    var output = document.getElementById('friendDiv');
+	  var div = document.createElement('div');
+	  var pName = document.createElement('p');
+	  var ulEmails = document.createElement('ul');
+	
+	  pName.innerText = contact['name'];
+	  div.appendChild(pName);
+	
+	  for (var j = 0, email; email = contact['emails'][j]; j++) {
+	    var liEmail = document.createElement('li');
+	    liEmail.innerText = email;
+	    ulEmails.appendChild(liEmail);
+	  }
+	
+	  div.appendChild(ulEmails);
+	  output.appendChild(div);             
+                
+                  
+       }
+      
           });
     });
   });
