cb6e7527aaa42fdec6a743527e2d0dd812a23464
juanmosv <3450624+juanmosv@users.noreply.github.com>
2018-03-10 15:07:24 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+var app = angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+app.controller('mainCtrl',function($scope) {
+
+angular.element(document).ready(function () {
+$scope.getData();
+});
+    $scope.data = {
+      content : ''
+    };
+    $scope.data = [];
+    ;
+    
+    $scope.gotData = function(data){
+ 
+      $scope.data = JSON.parse(data);
+      $scope.$apply();
+      
+    };
+    
+    $scope.leads = [
+    { id: 1, type: 'Call' },
+    { id: 2, type: 'Cold Call' },
+    { id: 3, type: 'Walk-in' },
+    { id: 3, type: 'Internet' },
+    { id: 3, type: 'Archive' }
+  ];
+   
+   $scope.selectedLeadCall = { id: 1, type: 'Call' };
+   $scope.selectedLeadCCal = { id: 2, type: 'Cold Call' };
+   $scope.selectedLeadWI = { id: 3, type: 'Walk-in' };
+   $scope.selectedLeadIn = { id: 4, type: 'Internet' };
+    
+    $scope.getData = function(){
+      google.script.run.withSuccessHandler($scope.gotData).getContacts();
+    };
+  
+  })
+  .config(function($mdThemingProvider) {
+
+    // Configure a dark theme with primary foreground yellow
+
+    $mdThemingProvider.theme('docs-dark', 'default')
+      .primaryPalette('yellow')
+      .dark();
+});
